8c5859ed3e9eae27ccc707e7f78a66d0d117fe05
Marc Rodriguez <marcrodrimartin@gmail.com>
2024-02-08 21:45:10 +0100

    Changed login and register styles.

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -1,4 +1,7 @@
 <x-guest-layout>
+    <div class="flex justify-center">
+        <x-application-logo class="w-20 h-20 fill-current text-gray-500" />
+    </div>
     <form method="POST" action="{{ route('register') }}">
         @csrf
 
@@ -39,14 +42,25 @@
             <x-input-error :messages="$errors->get('password_confirmation')" class="mt-2" />
         </div>
 
-        <div class="flex items-center justify-end mt-4">
-            <a class="underline text-sm text-gray-600 dark:text-gray-400 hover:text-gray-900 dark:hover:text-gray-100 rounded-md focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500 dark:focus:ring-offset-gray-800" href="{{ route('login') }}">
-                {{ __('Already registered?') }}
-            </a>
+        <div class="flex justify-center items-center mt-8">
 
-            <x-primary-button class="ms-4">
+            <x-primary-button class="ms-3 bg-green-700">
                 {{ __('Register') }}
             </x-primary-button>
         </div>
+
+        <div class="flex justify-center items-center mt-7">
+            @if (Route::has('login'))
+                <p
+                    class=" text-sm text-gray-600 dark:text-gray-400 hover:text-gray-900 dark:hover:text-gray-100 rounded-md focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500 dark:focus:ring-offset-gray-800">
+                    Already have an account?
+                    <a class="underline text-sm text-gray-600 dark:text-gray-400 hover:text-gray-900 dark:hover:text-gray-100 rounded-md focus:outline-none dark:focus:ring-offset-gray-800"
+                        href="{{ route('login') }}">
+                        {{ __('Sign in') }}
+                </p>
+                </a>
+            @endif
+        </div>
     </form>
 </x-guest-layout>
+
