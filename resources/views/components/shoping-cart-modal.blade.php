60ad7b1083e3406888e1bebab5a7b88f1e839859
Marc Rodriguez <marcrodrimartin@gmail.com>
2024-02-11 22:21:14 +0100

    Created Shopping Cart modal on index.

---
+++ resources/views/components/shoping-cart-modal.blade.php
@@ -0,0 +1,21 @@
+
+<div id="shopping-cart-modal" class="h-100 w-3/12 z-50 bg-white shadow-2xl fixed top-0 right-0 hidden">
+    <div class="close flex justify-end">
+        <button type="button"
+        class="text-gray-400 bg-transparent hover:bg-gray-200 hover:text-gray-900 rounded-lg text-sm w-8 h-8 ms-auto inline-flex justify-center items-center dark:hover:bg-gray-600 dark:hover:text-white"
+        data-modal-hide="static-modal" id="close-shopping-cart">
+        <svg class="w-3 h-3" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 14 14">
+            <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
+                d="m1 1 6 6m0 0 6 6M7 7l6-6M7 7l-6 6" />
+        </svg>
+        <span class="sr-only">Close modal</span>
+    </button>
+    </div>
+
+    <div class="message-empty w-100 flex flex-col justify-center items-center gap-5 ">
+        <h2>Tu carrito está vacío</h2>
+        <x-primary-button class="ms-3 bg-green-700 pt-3 pb-3 pl-8 pr-8">
+            {{ __('Seguir Comprando') }}
+        </x-primary-button>
+    </div>
+</div>
