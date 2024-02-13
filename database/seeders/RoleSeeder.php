e8b2aef8bbadb954ca6d98f98aed4882a5d86997
marcrodriguezm8 <m.rodriguez.cefpnuria@gmail.com>
2024-02-13 19:28:23 +0100

    Create Role and User Seeders.

---
+++ database/seeders/RoleSeeder.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\Role;
+use Illuminate\Database\Seeder;
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+
+class RoleSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        //
+        $arrayRoles = ['admin', 'user'];
+
+        foreach($arrayRoles as $role){
+            $roleClass = new Role;
+            $roleClass->name = $role;
+            $roleClass->save();
+        }
+
+    }
+}
