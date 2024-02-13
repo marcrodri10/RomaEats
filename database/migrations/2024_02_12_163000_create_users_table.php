1765d97c7d2af0fb94330f98f0347598289fe9df
marcrodriguezm8 <m.rodriguez.cefpnuria@gmail.com>
2024-02-13 19:29:47 +0100

    Modified users migration.

--- database/migrations/2024_02_12_163000_create_users_table.php
+++ database/migrations/2024_02_12_163000_create_users_table.php
@@ -14,7 +14,7 @@ return new class extends Migration
         Schema::create('users', function (Blueprint $table) {
             $table->id('user_id');
             $table->string('name');
-            $table->string('apellidos');
+            $table->string('surname');
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
