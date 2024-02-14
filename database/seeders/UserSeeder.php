8945d3d3a207a16030a11de2b93bc99336e0daff
marcrodriguezm8 <m.rodriguez.cefpnuria@gmail.com>
2024-02-14 19:41:26 +0100

    Create admin user on user seeder and call it on database.

--- database/seeders/UserSeeder.php
+++ database/seeders/UserSeeder.php
@@ -2,8 +2,10 @@
 
 namespace Database\Seeders;
 
-use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use App\Models\User;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 
 class UserSeeder extends Seeder
 {
@@ -13,5 +15,12 @@ class UserSeeder extends Seeder
     public function run(): void
     {
         //
+        $adminUser = new User;
+        $adminUser->name = 'marc.admin';
+        $adminUser->surname = "Admin";
+        $adminUser->password = Hash::make('12345');
+        $adminUser->email = 'marc.admin@gmail.com';
+        $adminUser->role_id = 1;
+        $adminUser->save();
     }
 }
