87084a1bb9bbd4f9bfa239b713280c40eb68a8fb
marcrodriguezm8 <m.rodriguez.cefpnuria@gmail.com>
2024-02-20 19:27:03 +0100

    Set pager to dishes.

--- app/Http/Controllers/DishController.php
+++ app/Http/Controllers/DishController.php
@@ -3,13 +3,15 @@
 namespace App\Http\Controllers;
 
 use App\Models\Dish;
-use Illuminate\Http\Request;
 
 class DishController extends Controller
 {
-    //
+    // Tonto quien lo lea
     function index(){
-        $dishes = Dish::all();
-        return view('dishes', ['dishes' => $dishes]);
+        $dishes = Dish::orderBy('dish_name', 'ASC')
+            ->with('ingredients')
+            ->paginate(5);
+        return view('dishes', compact('dishes'));
     }
+    // Por aquí también
 }
