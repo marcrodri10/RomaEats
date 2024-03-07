33757e68002e3dda0782d87a3a8dc8b753810727
Marc Rodriguez <marcrodrimartin@gmail.com>
2024-03-07 16:56:32 +0100

    Added new css and js to general functionalities.

--- resources/js/app.js
+++ resources/js/app.js
@@ -7,75 +7,62 @@ window.Alpine = Alpine;
 
 Alpine.start();
 
-const shopCartIcon = document.querySelectorAll('#shopping-cart > *');
 
+const shopCartIcon = document.querySelectorAll('#shopping-cart > *');
+const cartMessage = document.querySelector('.cart-message');
 const shopCartModal = document.querySelector('#shopping-cart-modal');
 const closeCart = document.querySelectorAll('#close-cart > *');
-const main = document.querySelector('main');
 
+let addedBuyButton = false;
+
+export let userShopCart = {
+
+};
+
+
+if (library.checkJSON(JSON.parse(localStorage.getItem('userShopCart'))) && library.checkJSON(JSON.parse(localStorage.getItem('userShopCart')))) {
+    cartMessage.innerHTML = ``;
+    //console.log(window.location);
+    userShopCart = JSON.parse(localStorage.getItem('userShopCart'));
+
+    library.createShopCartCard(userShopCart, cartMessage)
+
+    if (!addedBuyButton) {
+        const finishBuyDiv = library.createElement('div');
+        const buyButton = library.createElement('button', {
+            className: "ms-3 bg-green-700 pt-4 pb-4 pl-10 pr-10 flex justify-center mt-16 inline-flex items-center px-4 py-2 border border-transparent rounded-md font-semibold text-xs text-white dark:text-gray-800 uppercase tracking-widest hover:bg-green-800 dark:hover:bg-white  active:bg-gray-900 dark:active:bg-gray-300 focus:outline-none dark:focus:ring-offset-gray-800 transition ease-in-out duration-15",
+            textContent: 'FINALIZAR COMPRA',
+            id: "buy",
+        });
+
+        finishBuyDiv.appendChild(buyButton);
+        shopCartModal.appendChild(finishBuyDiv);
+
+        addedBuyButton = true;
+
+        const addOrderBtn = document.querySelector('#buy');
+
+        addOrderBtn.addEventListener('click', async () => {
+            const response = await library.sendDataToPhp('/addOrder', userShopCart);
+            if (response.message == 'Saved') window.location.href = '/order';
+        });
+    }
+
+
+}
 shopCartIcon.forEach(element => {
     element.addEventListener('click', (e) => {
         library.showModal(shopCartModal);
-        main.style.filter = 'blur(5px)';
+
     })
 
 })
 closeCart.forEach(element => {
     element.addEventListener('click', (e) => {
         library.hideModal(shopCartModal);
-        main.style.filter = 'blur(0)';
+
     })
 })
 
-const dishes = document.querySelector('#dishes');
-let userShopCart = {
-
-};
-
-dishes.addEventListener('click', (e) => {
-    var foodDish = e.target.closest('.food-dish');
-
-    var dishInfo = e.target.closest('.dish-info');
-
-    if (e.target.id == "add") {
-        console.log(e.target.nextSibling.nextSibling);
-        e.target.nextSibling.nextSibling.classList.remove('hidden');
-    }
-    else if (e.target.closest('#increment-button')) {
-        let button = e.target.closest('#increment-button');
-        button.parentNode.children[1].value++;
-
-        var incrementor = e.target.closest('.incrementor');
-        console.log(dishInfo);
-
-        shopCartModal.children[1].innerHTML = '';
-
-        userShopCart[foodDish.id] = {
-            name: dishInfo.children[0].textContent.trim(),
-            price: dishInfo.children[1].textContent.trim(),
-            quantity: incrementor.children[1].value,
-        }
-
-        for(let cart in userShopCart){
-            shopCartModal.children[1].innerHTML += `
-            <div class="flex w-90 justify-around items-center gap-3">
-                <div class="w-40">
-                    <img src = "img/landing1.jpeg">
-                </div>
-                <div class="w-40">
-                    <p>${userShopCart[cart].name}</p>
-                    <p>${userShopCart[cart].price}</p>
-                    <p>Cantidad: ${userShopCart[cart].quantity}</p>
-                </div>
-            </div>
-            `
-        }
-    }
-    else if (e.target.id == 'decrement-button' || e.target.parentNode.id == 'decrement-button') {
-        let button = e.target.closest('#decrement-button');
-        if (button.parentNode.children[1].value > 0) button.parentNode.children[1].value--;
-
-    }
-})
 
 
