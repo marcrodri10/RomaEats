db997cab20ef278c78c2129d83c6cbb12afdf49b
Marc Rodriguez <marcrodrimartin@gmail.com>
2024-03-07 16:40:57 +0100

    Created User and Employee factory.

---
+++ database/factories/EmployeeFactory.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace Database\Factories;
+
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+/**
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Model>
+ */
+class EmployeeFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+    public function definition(): array
+    {
+        return [
+            //
+            'hire_date' => fake()->dateTimeBetween('-5 year', 'now'),
+            'schedule' => fake()->randomElement(['morning', 'afternoon']),
+        ];
+    }
+}
--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -24,10 +24,25 @@ class UserFactory extends Factory
     public function definition(): array
     {
         return [
-            'name' => fake()->name(),
-            'email' => fake()->unique()->safeEmail(),
+            'name' => fake('es_ES')->firstName(),
+            'surname' => fake('es_ES')->lastName(),
+            'email' => fake('es_ES')->unique()->safeEmail(),
             'email_verified_at' => now(),
             'password' => static::$password ??= Hash::make('password'),
+            'address' => fake()->randomElement(array(
+                "Carrer de Sant Antoni, 23, 1r 1a",
+                "Avinguda Diagonal, 145, 2n 3a",
+                "Carrer dels Encants, 76, Principal 2a",
+                "Rambla de Catalunya, 89, Entresòl 1r",
+                "Travessera de Gràcia, 201, Àtic 2n",
+                "Carrer de la Marina, 37, Baixos",
+                "Passeig de Sant Joan, 112, 3r 2a",
+                "Carrer del Consell de Cent, 59, Local 1r",
+                "Gran Via de les Corts Catalanes, 415, 4t 1a",
+                "Carrer de Balmes, 199, 1r 2a"
+            )),
+            'phone' => fake('es_ES')->phoneNumber(),
+            'role_id' => 2,
             'remember_token' => Str::random(10),
         ];
     }
