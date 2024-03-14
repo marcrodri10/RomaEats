2ef622607814a017aa55905e0d6ddb320649e1c3
Marc Rodriguez <marcrodrimartin@gmail.com>
2024-03-14 13:49:24 +0100

    Modified some routes.

--- routes/web.php
+++ routes/web.php
@@ -26,11 +26,12 @@ use App\Http\Controllers\OrderDishController;
 Route::get('/', [IndexController::class, 'index'])->name('index');
 Route::get('/dishes', [DishController::class, 'index'])->name('dishes');
 Route::get('/dish/{id}', [DishController::class, 'showDish'])->name('dishes.show');
+Route::get('/getAllDishes', [DishController::class, 'getAllDishes'])->name('dishes.get');
 
 Route::get('/dashboard', function () {
     return view('dashboard');
 })->middleware(['auth', 'verified'])->name('dashboard');
-
+Route::post('/addOrder', [OrderDishController::class, 'addOrder'])->name('order.add');
 Route::middleware('auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
@@ -45,20 +46,19 @@ Route::middleware('auth')->group(function () {
     Route::post('/save-product', [ProductController::class, 'storeProduct'])->name('save.product');
     Route::get('/product/{id}', [ProductController::class, 'showProduct'])->name('product.show');
     Route::get('/getProducts', [ProductController::class, 'getAllProducts'])->name('product.getAll');
-    Route::post('/addOrder', [OrderDishController::class, 'addOrder'])->name('order.add');
     Route::get('/order', [OrderDishController::class, 'index'])->name('order.index');
     Route::get('/order/{id}', [OrderController::class, 'showOrder'])->name('order.show');
     Route::post('/payment', [PaymentController::class, 'index'])->name('payment.index');
     Route::post('/payment/pay', [PaymentController::class, 'pay'])->name('payment.pay');
     Route::get('/myorders', [OrderController::class, 'index'])->name('myorders.index');
-
+    Route::get('/myorders', [OrderController::class, 'index'])->name('myorders.index');
 });
 
 Route::middleware(['auth', 'employee',])->group(function(){
     Route::get('/orders', [OrderController::class, 'orders'])->name('employee.orders');
     Route::get('/allOrderAddress', [OrderController::class, 'getAllOrderAddress'])->name('employee.orders.getAllOrderAddress');
     Route::post('/route/{id}', [OrderController::class, 'showOrderMap'])->name('employee.orders.showOrderMap');
-    Route::get('/order/deliveryRoute', [OrderController::class, 'showDeliveryRoute'])->name('employee.orders.deliveryRoute');
+    Route::get('/deliveryRoute', [OrderController::class, 'showDeliveryRoute'])->name('employee.orders.deliveryRoute');
 });
 
 require __DIR__.'/auth.php';
