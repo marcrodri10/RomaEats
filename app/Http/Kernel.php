73137e3d0cde09f32d219178db718ab2640ae73b
Marc Rodriguez <marcrodrimartin@gmail.com>
2024-03-16 21:10:59 +0100

    Created User Middleware.

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -65,5 +65,6 @@ class Kernel extends HttpKernel
         'throttle' => \Illuminate\Routing\Middleware\ThrottleRequests::class,
         'verified' => \Illuminate\Auth\Middleware\EnsureEmailIsVerified::class,
         'employee' => \App\Http\Middleware\CheckEmployee::class,
+        'user' => \App\Http\Middleware\CheckUser::class,
     ];
 }
